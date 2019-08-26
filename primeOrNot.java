92be57ec99c1c3a064e0ecf12114761a55b4650a
Himanshu Tawra <53220024+himanshutawra666@users.noreply.github.com>
2019-08-27 01:17:20 +0530

    Update primeOrNot.java

--- primeOrNot.java
+++ primeOrNot.java
@@ -1,52 +1,53 @@
-import java.util.Scanner;
+import java.util.Scanner ;
+
 public class prime {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int num;
 		
-		boolean isPrime = true;
+		int num ;
 		
-		Scanner input = new Scanner(System.in);
+		boolean isPrime = true ;
 		
-		System.out.println("Enter a number");
-		num = input.nextInt();
+		Scanner input = new Scanner(System.in) ;
+		
+		System.out.println( " Enter a number " ) ;
+		
+		num = input.nextInt() ;
 		
 		if(num <= 0)
 		{
-			System.out.println("Enter a positive number");
+			System.out.println( " Enter a positive number " ) ;
 		}
 		
 		else
 		{
 			if(num == 1)
 			{
-				System.out.println("1 is not a prime number");
+				System.out.println( " 1 is not a prime number " ) ;
 			}
 			else
 			{
-				int temp;
-				for(int i = 2; i <= num/2; i++)
+		 		int temp ;
+				
+				for(int i = 2 ; i <= num/2 ; i++)
 				{
-					temp = num % i;
+					temp = num % i ;
+				
 					if(temp == 0)
 					{
-						isPrime = false;
+						isPrime = false ;
 					}
 					
 				}
 				if(isPrime)
 				{
-					System.out.println(num + " is a prime number");
+					System.out.println(num + " is a prime number ") ;
 				}
 				else
 				{
-					System.out.println(num + " is not a prime number");
+					System.out.println(num + " is not a prime number " ) ;
 				}
 			}
 		}
-		
-
 	}
-
 }
