7886096afb27d037f664df87b102e169dc523391
Himanshu Tawra <53220024+himanshutawra666@users.noreply.github.com>
2019-08-27 01:12:16 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,33 +1,32 @@
 import java.util.Scanner;
+
 public class factorial {
 
 	public static void main(String[] args) {
 		
-		int num;
-		int factorial = 1;
-		Scanner input = new Scanner(System.in);
+		int num ;
 		
-		System.out.println("Enter a number");
-		num = input.nextInt();
+		int factorial = 1 ;
+		
+		Scanner input = new Scanner(System.in) ;
+		
+		System.out.println( " Enter a number " ) ;
+		
+		num = input.nextInt() ;
 		
 		if(num <= 0)
 		{
-			System.out.println("Enter a positive number");
+			System.out.println( " Enter a positive number " ) ;
 		}
 				
 		else
 		{
-			for(int i = 1; i <= num; i++)
+			for(int i = 1 ; i <= num ; i++)
 			{
-				factorial = factorial * i;
+				factorial = factorial * i ;
 			}
 			
-			System.out.println("Factorial of " + num + " : " + factorial);
-			
-			
-			
-			
+			System.out.println(" Factorial of " + num + " : " + factorial ) ;	
 		}
 	}
-
 }
