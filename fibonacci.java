dfefb83139b888a781b14af79a14899806cd2478
Himanshu Tawra <53220024+himanshutawra666@users.noreply.github.com>
2019-08-27 01:07:36 +0530

    Create  fibonacci.java

---
+++ fibonacci.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class fibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num,first = 0 , second = 1, third;
+		System.out.println("Enter the number of terms ");
+		
+		Scanner input = new Scanner(System.in);
+		
+		num = input.nextInt();
+		
+		if(num <= 0)
+		{
+			System.out.println("Enter a positive number");
+		}
+		else
+		{
+			System.out.print(first + " , " + second);
+			for(int i = 2; i < num; i++ )
+			{	
+				third = first + second;
+				System.out.print(" , " + third);
+				first = second;
+				second = third;
+				
+			}
+			
+			
+		}
+		
+		
+		
+	}
+
+}
