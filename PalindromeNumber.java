32348bf96f97ce20bb22f4155c24c3b4f0190a55
Himanshu Tawra <53220024+himanshutawra666@users.noreply.github.com>
2019-08-29 01:08:36 +0530

    Create PalindromeNumber.java

---
+++ PalindromeNumber.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class PalindromeNum {
+
+	public static void main(String[] args) {
+		
+		
+		int num,rem,rev = 0;
+		
+    System.out.println("Enter a Number");
+		
+		Scanner input = new Scanner(System.in);
+		
+		num = input.nextInt();
+		
+		int temp = num;
+		
+		while(temp != 0)
+		{
+			rem = temp % 10;
+			
+      rev = rev * 10 + rem;
+			
+      temp = temp / 10;
+			
+		}
+		
+    if(num == rev)
+		
+    {
+			
+      System.out.println( num +" is a Palindrome Number");
+		
+    }
+		else
+		{
+			System.out.println( num +" is not a Palindrome Number");
+			
+		}
+		
+	}
+
+}
