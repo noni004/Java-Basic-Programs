33f17e18362d1069cb9e346179cf8da4a76a6df2
Himanshu Tawra <himanshu.tawra01@gmail.com>
2019-10-20 18:35:25 +0530

    Create stack.java

---
+++ stack.java
@@ -0,0 +1,53 @@
+// A java program to implement Stack concept using Java Collection Framework
+
+import java.io.*;
+import java.util.*;
+
+public class MyProgram {
+
+	static void stack_push(Stack<Integer>MyStack,int i)
+	{
+		MyStack.push(i);
+	}
+	
+	static void stack_pop(Stack<Integer>MyStack)
+	{
+		System.out.println("Pop :" + (Integer)MyStack.pop());
+	}
+	static void stack_peek(Stack<Integer>MyStack)
+	{
+		System.out.println("Element on Top: " + (Integer)MyStack.peek());
+	}
+	
+	static void stack_search(Stack<Integer> MyStack, int ele)
+	{
+		Integer pos = (Integer)MyStack.search(ele);
+		if(pos == -1)
+			System.out.println("Element not found");
+		else
+			System.out.println("Element position: " + pos);
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		Stack<Integer>MyStack = new Stack<Integer>();
+		stack_push(MyStack,2);
+		stack_push(MyStack,4);
+		stack_push(MyStack,6);
+		stack_push(MyStack,7);
+		stack_pop(MyStack);
+		stack_push(MyStack,8);
+		stack_peek(MyStack);
+		stack_search(MyStack,2);
+		stack_search(MyStack,6);
+		stack_search(MyStack,13);
+		
+		
+		
+		
+		
+
+	}
+
+}
